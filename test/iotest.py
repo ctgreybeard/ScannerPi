04ef003e4bdf96779d18a43cac639c988251da0e
Bill Waggoner <ctgreybeard@gmail.com>
2014-06-29 15:05:11 -0400

    Working loop

--- test/iotest.py
+++ test/iotest.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
+import codecs
 import subprocess
 from threading import Thread
 from time import sleep
+import inspect
 
 dev = "/dev/ttyUSB0"
 
@@ -11,30 +14,48 @@ rc = subprocess.call("stty --file {} 115200 -echo -ocrnl -onlcr raw time 5".form
 if rc != 0:
 	raise IOError("Unable to initialize \"{}\"".format(dev))
 
+def decodeerror(decodeerror):
+	return ('?' * (decodeerror.end - decodeerror.start), decodeerror.end, )
+
+codecs.register_error('scanner', decodeerror)
+
+running = True
+
 def putit():
-	writeio = None
-	while True:
-		sleep(0.2)
-		#print("Writing...")
-		if writeio is None or writeio.closed:
-			writeio = open(dev, mode = 'wt', buffering = 1, newline = '\r', encoding = 'ascii')
-		writeio.write("STS\r")
-	writeio.close()
+	try:
+		writeio = None
+		while running:
+			sleep(1)
+			#print("Writing...")
+			if writeio is None or writeio.closed:
+				writeio = open(dev, mode = 'wt', buffering = 1, newline = '\r', encoding = 'ascii')
+			writeio.write("GLG\r")
+	finally:
+		writeio.close()
 
 def getit():
-	readio = None
-	while True:
-		#print("Reading...")
-		if readio is None or readio.closed:
-			readio = open(dev, mode = 'rt', buffering = 1, newline = '\r', encoding = 'ascii', errors = 'replace')
-		r = readio.readline().rstrip('\r')
-		#print("Received: \"{}\"".format(r.rstrip('\r')))
-		s = r.split(',')
-		print(','.join(s[4:13:2]))
-	readio.close()
+	try:
+		readio = None
+		while running:
+			#print("Reading...")
+			if readio is None or readio.closed:
+				readio = open(dev, mode = 'rt', buffering = 1, newline = '\r', encoding = sys.argv[1], errors = 'scanner')
+			r = readio.readline().rstrip('\r')
+			print("Received: \"{}\"".format(r.rstrip('\r')))
+			#s = r.split(',')
+			#print(','.join(s[4:13:2]))
+	finally:
+		readio.close()
 
 writer = Thread(target=putit, name='Writer')
 reader = Thread(target=getit, name='Reader')
 
 reader.start()
 writer.start()
+try:
+	writer.join()
+except KeyboardInterrupt:
+	print("Stopping...")
+	running = False
+
+reader.join()
