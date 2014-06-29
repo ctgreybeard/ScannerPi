dce997d90e8a7d6028a238ccb2df7153b1c01207
Bill Waggoner <ctgreybeard@gmail.com>
2014-06-29 12:20:50 -0400

    Rapid read/write

--- test/iotest.py
+++ test/iotest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
-from threading import Thread, RLock
+from threading import Thread
 from time import sleep
 
 dev = "/dev/ttyUSB0"
@@ -12,21 +12,26 @@ if rc != 0:
 	raise IOError("Unable to initialize \"{}\"".format(dev))
 
 def putit():
+	writeio = None
 	while True:
-		sleep(5)
-		print("Writing...")
-		writeio = open(dev, mode = 'wt', buffering = 1, newline = '\r', encoding = 'ascii')
+		sleep(0.2)
+		#print("Writing...")
+		if writeio is None or writeio.closed:
+			writeio = open(dev, mode = 'wt', buffering = 1, newline = '\r', encoding = 'ascii')
 		writeio.write("STS\r")
-		writeio.close()
+	writeio.close()
 
 def getit():
+	readio = None
 	while True:
-		sleep(0.5)
-		print("Reading...")
-		readio = open(dev, mode = 'rt', buffering = 1, newline = '\r', encoding = 'ascii', errors = 'replace')
-		r = readio.readline()
-		print("Received: \"{}\"".format(r.rstrip('\r')))
-		readio.close()
+		#print("Reading...")
+		if readio is None or readio.closed:
+			readio = open(dev, mode = 'rt', buffering = 1, newline = '\r', encoding = 'ascii', errors = 'replace')
+		r = readio.readline().rstrip('\r')
+		#print("Received: \"{}\"".format(r.rstrip('\r')))
+		s = r.split(',')
+		print(','.join(s[4:13:2]))
+	readio.close()
 
 writer = Thread(target=putit, name='Writer')
 reader = Thread(target=getit, name='Reader')
