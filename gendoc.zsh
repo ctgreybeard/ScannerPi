0d2aa7bf460b8b534d216a4945f38d6e2a75b25f
Bill Waggoner <ctgreybeard@gmail.com>
2015-11-07 18:14:21 -0500

    Use the docs directory for the html and related files

--- gendoc.zsh
+++ gendoc.zsh
@@ -2,7 +2,7 @@
 
 for f in **/*.py; do
     name=$(basename $f .py )
-    pygmentize -f html -o ${name}.html \
+    pygmentize -f html -o docs/${name}.html \
         -P 'cssfile=pystyle.css' \
         -O 'style=colorful,linenos=inline,full,noclobber_cssfile=1' \
         -P "title=$f" \
