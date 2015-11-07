896d62f18272558d5aff9bab1679b54425373bd1
Bill Waggoner <ctgreybeard@gmail.com>
2015-11-07 12:36:51 -0500

    Better

--- gendoc.zsh
+++ gendoc.zsh
@@ -1,7 +1,7 @@
-#!/sw/bin/zsh -f
+#!zsh -f
 
 for f in **/*.py; do
-    name=$(basename -s .py $f)
+    name=$(basename $f .py )
     pygmentize -f html -o ${name}.html \
         -P 'cssfile=pystyle.css' \
         -O 'style=colorful,linenos=inline,full,noclobber_cssfile=1' \
